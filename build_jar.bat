dd054f805b89fb1084df56e4bfbc0c204c0fa1a7
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-03-29 17:14:51 +0200

    alotofitems

---
+++ build_jar.bat
@@ -0,0 +1,4 @@
+@echo off
+call gradlew.bat build
+echo opendir
+start "" "D:\Dev\Minecraft\RamCraft\build\libs"
--- src/main/java/net/yel0w/ramcraft/item/ModItems.java
+++ src/main/java/net/yel0w/ramcraft/item/ModItems.java
@@ -10,9 +10,15 @@ import net.yel0w.ramcraft.RamCraft;
 
 public class ModItems {
 
+
+
     public static final Item SILISIUM_DUST = RegisterItems("silicium_dust", new Item(new Item.Settings()));
-
-
+    public static final Item SILICON = RegisterItems("silicon", new Item(new Item.Settings()));
+    public static final Item CARBON_DUST = RegisterItems("carbon_dust", new Item(new Item.Settings()));
+    public static final Item TRANSISTOR = RegisterItems("transistor", new Item(new Item.Settings()));
+    public static final Item GERMANIUM = RegisterItems("germanium", new Item(new Item.Settings()));
+    public static final Item MEMORY_CELL = RegisterItems("memory_cell", new Item(new Item.Settings()));
+    public static final Item ALUMINUM_INGOT = RegisterItems("aluminum_ingot", new Item(new Item.Settings()));
 
 private static Item RegisterItems(String name, Item item) {
     return Registry.register(Registries.ITEM, Identifier.of(RamCraft.MOD_ID, name), item);
@@ -24,6 +30,12 @@ public static void registerModItems()
 
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
         entries.add(SILISIUM_DUST);
+        entries.add(SILICON);
+        entries.add(CARBON_DUST);
+        entries.add(TRANSISTOR);
+        entries.add(GERMANIUM);
+        entries.add(MEMORY_CELL);
+        entries.add(ALUMINUM_INGOT);
     });
 
 }
